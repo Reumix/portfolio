5791e58f84adc7cf90dab7ff219d432cfdf4806a
Reumiii <Remi.CHABRERIE@etu.uca.fr>
2023-02-25 17:14:42 +0100

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import Contact from "./components/Contact.jsx";
@@ -10,7 +10,7 @@ import Projet from "./components/Projet.jsx";
 
 const router = createBrowserRouter([
     {
-        path: '/',
+        path: '/testvite',
         element: <App/>,
     },
     {
